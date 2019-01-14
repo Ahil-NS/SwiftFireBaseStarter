589f13ea5ef1c3b782c776617436d558aaaa2b99
a <eahilendran@gmail.com>
2019-01-14 17:06:12 +0530

    Add read update delete and query data

--- SwiftFirebaseStarter/Controller/FoodMainTableVC.swift
+++ SwiftFirebaseStarter/Controller/FoodMainTableVC.swift
@@ -12,12 +12,50 @@ import Firebase
 class FoodMainTableVC: UITableViewController {
     
     var items: [FoodItem] = []
-    let groceryItemsReference = Database.database().reference(withPath: "food-items")
+    let foodItemsReference = Database.database().reference(withPath: "food-items")
     
     @IBOutlet var foodTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        //Get Child Values
+        //        foodItemsReference.child("pizza").observe(.value) { snapshot in
+        //            let values = snapshot.value as! [String:AnyObject]
+        //            let foodName = values["foodName"] as! String
+        //            let shopName = values["shopName"] as! String
+        //            let completed = values["completed"] as! Bool
+        //            print("chhh", foodName,shopName,completed)
+        //        }
+        
+        //Observe for value changed
+        
+        //        foodItemsReference.observe(.value) { (snapshot) in
+        //            var newItems : [FoodItem] = []
+        //            for item in snapshot.children{
+        //                let foodItem = FoodItem(snapshot: item as! DataSnapshot)
+        //                newItems.append(foodItem)
+        //            }
+        //            self.items = newItems
+        //            self.foodTableView.reloadData()
+        //        }
+        //
+        foodItemsReference.observe(.value, with: {
+            snapshot in
+            print(snapshot)
+        })
+        
+        
+        foodItemsReference.queryOrdered(byChild: "completed").observe(.value) { snapshot in
+            var newItems: [FoodItem] = []
+            for item in snapshot.children{
+                let foodItem = FoodItem(snapshot: item as! DataSnapshot)
+                newItems.append(foodItem)
+            }
+            self.items = newItems
+            self.foodTableView.reloadData()
+        }
         
     }
     
@@ -39,15 +77,15 @@ class FoodMainTableVC: UITableViewController {
                                         let textFieldFoodName = alert.textFields![0]
                                         let textFieldShopName = alert.textFields![1]
                                         
-                                        let groceryItem = FoodItem(foodName: textFieldFoodName.text!,
-                                                                   shopName: textFieldShopName.text!,
-                                                                   completed: false)
-                                        self.items.append(groceryItem)
+                                        let foodItem = FoodItem(foodName: textFieldFoodName.text!,
+                                                                shopName: textFieldShopName.text!,
+                                                                completed: false)
+                                        self.items.append(foodItem)
                                         self.foodTableView.reloadData()
                                         
-                                        let groceryItemRef = self.groceryItemsReference.child(textFieldFoodName.text!.lowercased())
+                                        let foodItemRef = self.foodItemsReference.child(textFieldFoodName.text!.lowercased())
                                         let values: [String: Any] = [ "foodName": textFieldFoodName.text!, "shopName": textFieldShopName.text!, "completed": false]
-                                        groceryItemRef.setValue(values)
+                                        foodItemRef.setValue(values)
                                         
         }
         
@@ -68,7 +106,60 @@ class FoodMainTableVC: UITableViewController {
         let FoodItem = items[indexPath.row]
         cell.textLabel?.text = FoodItem.foodName
         cell.detailTextLabel?.text = FoodItem.shopName
+        
+        toggleCellCheckbox(cell, isCompleted: FoodItem.completed)
+        
         return cell
+    }
+    
+    func toggleCellCheckbox(_ cell: UITableViewCell, isCompleted: Bool) {
+        if !isCompleted {
+            cell.accessoryType = .none
+            cell.textLabel?.textColor = UIColor.black
+            cell.detailTextLabel?.textColor = UIColor.black
+        } else {
+            cell.accessoryType = .checkmark
+            cell.textLabel?.textColor = UIColor.gray
+            cell.detailTextLabel?.textColor = UIColor.gray
+        }
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        ////Update Data
+        ////
+        ////        let values: [String:Any] = ["foodName" : "Ahillll"]
+        ////        foodItem.ref?.updateChildValues(values)
+        //
+        //        foodTableView.reloadData()
+        
+        guard let cell = tableView.cellForRow(at: indexPath) else { return }
+        var foodItem = items[indexPath.row]
+        let toggledCompletion = !foodItem.completed
+        
+        toggleCellCheckbox(cell, isCompleted: toggledCompletion)
+        foodItem.completed = toggledCompletion
+        foodItem.ref?.updateChildValues(["completed" : toggledCompletion])
+        
+        foodTableView.reloadData()
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if(editingStyle == .delete){
+            let foodItem = items[indexPath.row]
+            //foodItem.ref?.removeValue()
+            foodItem.ref?.setValue(nil)
+            
+            items.remove(at: indexPath.row)
+            foodTableView.reloadData()
+            
+        }
     }
 }
 
--- SwiftFirebaseStarter/Model/FoodItem.swift
+++ SwiftFirebaseStarter/Model/FoodItem.swift
@@ -7,17 +7,32 @@
 //
 
 import Foundation
+import Firebase
 
 struct FoodItem {
     
-    var foodName: String
-    var shopName: String
+    let foodName: String
+    let shopName: String
     var completed: Bool
+    let ref: DatabaseReference?
+    let key: String
     
-    init(foodName: String , shopName: String, completed: Bool) {
+    
+    init(foodName: String , shopName: String, completed: Bool, key: String = "") {
+        self.key = key
         self.foodName = foodName
         self.shopName = shopName
         self.completed = completed
+        self.ref = nil
+    }
+    
+    init(snapshot : DataSnapshot){
+        key = snapshot.key
+        ref = snapshot.ref
+        let snapshotValue = snapshot.value as! [String: AnyObject]
+        foodName = snapshotValue["foodName"] as! String
+        shopName = snapshotValue["shopName"] as! String
+        completed = snapshotValue["completed"] as! Bool
     }
     
 }
